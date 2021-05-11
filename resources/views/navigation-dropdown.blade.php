e40ac3df8ab1dd3bb0eae2c54c8718a3acffa58f
prajarisnaldy <prajarisnaldy06@gmail.com>
2021-05-11 22:44:40 +0700

    end

--- resources/views/navigation-dropdown.blade.php
+++ resources/views/navigation-dropdown.blade.php
@@ -1,17 +1,15 @@
 <nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
     <!-- Primary Navigation Menu -->
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div class="max-w-full mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
             <div class="flex">
                 <!-- Logo -->
-                <div class="flex-shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
-                        <x-jet-application-mark class="block h-9 w-auto" />
-                    </a>
+                <div class="flex-shrink-0 flex items-center text-lg font-extrabold px-1 mt-1 text-blue-900">
+                    SMANDARA.
                 </div>
 
                 <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex px-5">
                     <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                         <h1 class="text-base text-blue-900 hover:text-blue-600 font-bold">Home</h1>
                     </x-jet-nav-link>
