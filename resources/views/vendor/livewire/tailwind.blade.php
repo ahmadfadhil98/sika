730e79c20106966faebc30f226a6e580e09d3711
prajarisnaldy <prajarisnaldy06@gmail.com>
2021-07-28 08:08:01 +0700

    Praja_awsw

--- resources/views/vendor/livewire/tailwind.blade.php
+++ resources/views/vendor/livewire/tailwind.blade.php
@@ -30,13 +30,8 @@
             <div class="hidden sm:flex-1 sm:flex sm:items-center sm:justify-between">
                 <div>
                     <p class="text-sm text-gray-700 leading-5">
-                        <span>{!! __('Showing') !!}</span>
-                        <span class="font-medium">{{ $paginator->firstItem() }}</span>
-                        <span>{!! __('to') !!}</span>
-                        <span class="font-medium">{{ $paginator->lastItem() }}</span>
-                        <span>{!! __('of') !!}</span>
-                        <span class="font-medium">{{ $paginator->total() }}</span>
-                        <span>{!! __('results') !!}</span>
+                        <span>{!! __('Total data:') !!}</span>
+                        <span class="font-semibold text-blue-700">{{ $paginator->total() }}</span>
                     </p>
                 </div>
 
