c309c71255e258a1beb27ac658479bf304a1e668
prajarisnaldy <prajarisnaldy06@gmail.com>
2021-07-28 14:31:16 +0700

    Praja_editingStek

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -39,4 +39,42 @@
         <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.6.0/dist/alpine.js" defer></script>
 
     </body>
+
+    <div style="background-color: #F8F8FB;">
+        <footer>
+            <div class="bg-gray-200 max-w-full mx-auto px-4 sm:px-6 lg:px-8 rounded-tr-full">
+                <div class="w-full flex justify-between h-8 px-0.5">
+                </div>
+            </div>
+            <div class="bg-gray-200 max-w-full mx-auto px-4 sm:px-6 lg:px-8">
+                <div class="w-full flex justify-between h-16 px-0.5">
+                    <div class="w-full flex-shrink-0 flex items-center">
+                        <div class="w-2/3 text-gray-500 text-xs -mt-7">
+                                Copyright © Sistem Informasi Keuangan Asrama SMANDARA
+                        </div>
+                        <div class="w-1/3 justify-end -mt-7">
+                            <div class="text-gray-500 text-xs flex">
+                                <div class="flex text-red-600 rounded-full mb-1.5 underline">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="13" class="text-red-600 mr-1.5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                      </svg>
+                                    <div>
+                                        Peringatan
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="text-gray-500 text-xs">
+                                Bagi semua pihak yang mengakses sistem ini tanpa izin pengelola, maka akan dikenakan <span class="font-bold">sanksi yang berat</span>.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="bg-gray-900 max-w-full mx-auto px-4 sm:px-6 lg:px-8">
+                <div class="w-full flex justify-between h-5 px-0.5">
+                </div>
+            </div>
+        </footer>
+    </div>
+
 </html>
