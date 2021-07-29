b13fe5cb89783dd78e8bf54f6e05cf38b7bd54f9
prajarisnaldy <prajarisnaldy06@gmail.com>
2021-07-29 09:59:04 +0700

    Praja_Risnaldy_

--- resources/views/vendor/jetstream/components/validation-errors.blade.php
+++ resources/views/vendor/jetstream/components/validation-errors.blade.php
@@ -1,11 +1,12 @@
 @if ($errors->any())
     <div {{ $attributes }}>
-        <div class="font-medium text-red-600">{{ __('Whoops! Something went wrong.') }}</div>
+        <div class="pt-3 underline text-red-500">{{ __('Ops, Akses Ditolak!.') }}</div>
+        <div class="pt-1 text-red-500">*Pastikan Anda menggunakan <span class="underline">Nama Pengguna</span> dan <span class="underline">Kata Sandi</span> yang benar</div>
 
-        <ul class="mt-3 list-disc list-inside text-sm text-red-600">
+        {{-- <ul class="mt-3 list-disc list-inside text-sm text-red-600">
             @foreach ($errors->all() as $error)
                 <li>{{ $error }}</li>
             @endforeach
-        </ul>
+        </ul> --}}
     </div>
 @endif
